0e97fdf5ad6d7f52f71f88b8392969c71cbfc5be
최원혁 <whchoi000@gmail.com>
2019-10-29 14:14:16 +0900

    ver3.0.1

--- homework_2/lib/main.dart
+++ homework_2/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
-
-void main() => runApp(DscHufsTalk());
-const String _name = "DevLuce";
+import 'package:flutter/foundation.dart';
 
 final ThemeData kIOSTheme = new ThemeData(
-  primarySwatch: Colors.orange,
-  primaryColor: Colors.grey[100],
+  primarySwatch: Colors.pink,
+  primaryColor: Colors.grey[400],
   primaryColorBrightness: Brightness.light,
 );
 
@@ -16,40 +13,167 @@ final ThemeData kDefaultTheme = new ThemeData(
   accentColor: Colors.orangeAccent[400],
 );
 
+const String _name = "DevLuce";
+
+void main() {
+  runApp(new DscHufsTalk());
+}
+
 class DscHufsTalk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "DSC HUFS Talk",
-      theme: defaultTargetPlatform == TargetPlatform.iOS //new
-          ? kIOSTheme //new
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
           : kDefaultTheme,
-      home: new TalkScreen(),
+      home: new ChatScreen(),
     );
   }
 }
 
-class TalkScreen extends StatefulWidget {
-  @override
-  _TalkScreenState createState() => new _TalkScreenState();
+@override
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead.apply(color: Colors.purpleAccent)),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text, style: TextStyle(color: Colors.teal)),
+                  ),
+                ],
+              ),
+            ),
+            new Container(
+              margin: const EdgeInsets.only(left: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _TalkScreenState extends State<TalkScreen> with TickerProviderStateMixin {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
   bool _isComposing = false;
 
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title:
+              new Text("DSC HUFS Talk", style: TextStyle(color: Colors.blue)),
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Stack(children: <Widget>[
+          new Center(
+            child: new Image.asset(
+              'assets/dsc_logomark_color.png',
+              width: 300,
+              height: 300,
+              fit: BoxFit.fill,
+            ),
+          ),
+          new Column(children: <Widget>[
+            new Flexible(
+                child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )),
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ])
+        ]));
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: _isComposing ? _handleSubmitted : null,
+                decoration: new InputDecoration.collapsed(hintText: "메시지 입력"),
+              ),
+            ),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? new CupertinoButton(
+                        child: new ClipOval(
+                          child: Container(
+                            child: Icon(Icons.arrow_upward),
+                            color: Colors.blue[300],
+                            padding: const EdgeInsets.all(3),
+                            //decoration: ShapeDecoration(shape: CircleBorder()),
+                          ),
+                        ),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )
+                    : new IconButton(
+                        icon: new Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )),
+          ]),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border:
+                      new Border(top: new BorderSide(color: Colors.grey[300])))
+              : null),
+    );
+  }
+
   void _handleSubmitted(String text) {
     _textController.clear();
     setState(() {
-      //new
-      _isComposing = false; //new
+      _isComposing = false;
     });
     ChatMessage message = new ChatMessage(
       text: text,
       animationController: new AnimationController(
-        //new
-        duration: new Duration(milliseconds: 700), //new
+        duration: new Duration(milliseconds: 300),
         vsync: this,
       ),
     );
@@ -61,135 +185,8 @@ class _TalkScreenState extends State<TalkScreen> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    //new
-    for (ChatMessage message in _messages) //new
-      message.animationController.dispose(); //new
-    super.dispose(); //new
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("DSC HUFS Talk"),
-          elevation:
-              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0),
-      body: new Container(
-          child: new Column(
-            //modified
-            children: <Widget>[
-              //new
-              new Flexible(
-                //new
-                child: new ListView.builder(
-                  //new
-                  padding: new EdgeInsets.all(8.0), //new
-                  reverse: true, //new
-                  itemBuilder: (_, int index) => _messages[index], //new
-                  itemCount: _messages.length, //new
-                ), //new
-              ), //new
-              new Divider(height: 1.0), //new
-              new Container(
-                //new
-                decoration:
-                    new BoxDecoration(color: Theme.of(context).cardColor), //new
-                child: _buildTextComposer(), //modified
-              ), //new
-            ], //new
-          ),
-          decoration: Theme.of(context).platform == TargetPlatform.iOS //new
-              ? new BoxDecoration(
-                  //new
-                  border: new Border(
-                    //new
-                    top: new BorderSide(color: Colors.grey[200]), //new
-                  ), //new
-                ) //new
-              : null),
-    );
-  }
-
-  Widget _buildTextComposer() {
-    return new IconTheme(
-      //new
-      data: new IconThemeData(color: Theme.of(context).accentColor), //new
-      child: new Container(
-        //modified
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
-          children: <Widget>[
-            new Flexible(
-              child: new TextField(
-                controller: _textController,
-                onSubmitted: _handleSubmitted,
-                decoration: new InputDecoration.collapsed(hintText: "메시지 입력"),
-              ),
-            ),
-            new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: Theme.of(context).platform == TargetPlatform.iOS
-                  ? //modified
-                  new CupertinoButton(
-                      //new
-                      child: new Text("Send"), //new
-                      onPressed: _isComposing //new
-                          ? () => _handleSubmitted(_textController.text) //new
-                          : null,
-                    )
-                  : new IconButton(
-                      icon: new Icon(Icons.send),
-                      onPressed: _isComposing
-                          ? () =>
-                              _handleSubmitted(_textController.text) //modified
-                          : null,
-                    ),
-            ),
-          ],
-        ),
-      ), //new
-    );
-  }
-}
-
-class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.animationController});
-  final String text;
-  final AnimationController animationController;
-  @override
-  Widget build(BuildContext context) {
-    return new SizeTransition(
-        //new
-        sizeFactor: new CurvedAnimation(
-            //new
-            parent: animationController,
-            curve: Curves.easeOut), //new
-        axisAlignment: 0.0, //new
-        child: new Container(
-          //modified
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                margin: const EdgeInsets.only(right: 16.0),
-                child: new CircleAvatar(child: new Text(_name[0])),
-              ),
-              new Expanded(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text(_name, style: Theme.of(context).textTheme.subhead),
-                    new Container(
-                      margin: const EdgeInsets.only(top: 5.0),
-                      child: new Text(text),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ) //new
-        );
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
   }
 }
