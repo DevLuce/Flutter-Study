d49643fd8512664ee2554e537f07b6d97957d6f6
최원혁 <whchoi000@gmail.com>
2019-11-22 10:07:21 +0900

    story app

---
+++ clone_story_app/lib/customIcons.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData menu = IconData(0xe900, fontFamily: "CustomIcons");  //폰트를 이용해서 커스텀 아이콘 생성, 0xe900은 codepoint
+  static const IconData option = IconData(0xe902, fontFamily: "CustomIcons");  
+}
